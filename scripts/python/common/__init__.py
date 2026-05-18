7d6dd8fe33d935d0d592c6a651e04462c346d102
Oak-22 <buccatjulian@gmail.com>
2026-05-18 15:37:40 -0700

    Expose shared Python IO helpers

--- scripts/python/common/__init__.py
+++ scripts/python/common/__init__.py
@@ -1,2 +1,5 @@
 """Shared utilities for workflow-observability scripts."""
 
+from .io_utils import ensure_parent_dir, read_json, write_json
+
+__all__ = ["ensure_parent_dir", "read_json", "write_json"]
